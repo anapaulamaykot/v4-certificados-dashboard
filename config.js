41517762322bae7c6ad762907034551a544fdac4
anapaulamaykot <anapaulamaykot.apm@gmail.com>
2026-07-15 07:41:01 -0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Configuração do Supabase — usada pelo admin.html e pelo index.html
-const SUPABASE_URL = "https://vfikegzrbdgtxbjzuekm.supabase.co";
-const SUPABASE_KEY = "sb_publishable_BUwgLA5MoPlzTPE9GxqokQ_S_Ic7ewl";
